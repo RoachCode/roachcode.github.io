f02cee085ed6f23bf804723ab33cfd22fdc662ea
Bradley <74122730+RoachCode@users.noreply.github.com>
2023-06-21 19:20:41 -0700

    at least there's a basic theme?

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
 function toggleTheme()
 {
+    const button = document.getElementById('toggle-theme-button');
     if (document.body.dataset.theme === 'dark')
     {
         document.body.dataset.theme = 'light';
+        button.innerText = 'Enable Dark Mode';
     }
     else
     {
         document.body.dataset.theme = 'dark'
+        button.innerText = 'Enable Light Mode';
     }
 }
 
 function init()
 {
-    document.body.dataset.theme = 'light';
+    document.body.dataset.theme = 'light';    
+    button.innerText = 'Enable Dark Mode';
     document.getElementById('toggle-theme-button').addEventListener('mouseup', toggleTheme);
 }
 
