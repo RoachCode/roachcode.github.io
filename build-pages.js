92e5cc33882c2230368a8ab6ef8a678006ca6be4
Bradley <74122730+RoachCode@users.noreply.github.com>
2025-03-15 17:58:41 -0700

    custom scrollbar. is a little slow.

--- build-pages.js
+++ build-pages.js
@@ -1,5 +1,4 @@
-function buildHome()
-{
+function buildHome() {
     // Creates main container in light mode
     const mainContainer = document.getElementById('main-container');
     const homeContainer = document.createElement('div');
@@ -40,48 +39,43 @@ function buildHome()
     mainContainer.append(homeContainer);
 };
 
-function buildProjects()
-{
+function buildProjects() {
     // Handles creation and style of elements.
     // This is styled automatically so that changes are easy
     // And scalable, and nothing breaks the DOM.
-    class Project
-    {
-        constructor(imageId, pText, fText)
-        {
+    class Project {
+        constructor(title, imageId, pText, fText) {
             this.elementArray = [];
 
             const paragraphCount = pText.pText.length;
-            for (let i = 0; i < paragraphCount; ++i)
-            {
+            for (let i = 0; i < paragraphCount; ++i) {
                 this.paragraph = document.createElement('p');
                 this.paragraph.innerText = pText.pText[i];
-                this.elementArray.push(this.paragraph);                
+                this.elementArray.push(this.paragraph);
             }
 
             const imageCount = imageId.imageId.length;
-            for (let i = 0; i < imageCount; ++i)
-            {
+            for (let i = 0; i < imageCount; ++i) {
                 this.image = document.createElement('div');
                 this.image.setAttribute('id', imageId.imageId[i]);
+                this.image.classList.add('is-image');
                 this.image.style.width = '90%';
                 this.image.style.height = '50vh';
                 this.image.style.backgroundSize = 'cover';
                 this.image.style.placeSelf = 'center';
+                this.image.title = title.title[i];
                 this.elementArray.push(this.image);
             }
 
             const footerCount = fText.fText.length;
-            for (let i = 0; i < footerCount; ++i)
-            {
+            for (let i = 0; i < footerCount; ++i) {
                 this.footer = document.createElement('p');
                 this.footer.classList.add('fine-print', 'image-footer');
                 this.footer.innerText = fText.fText[i];
                 this.elementArray.push(this.footer);
             }
         }
-        getElements()
-        {
+        getElements() {
             return this.elementArray;
         }
     }
@@ -95,32 +89,40 @@ function buildProjects()
     projectsContainer.dataset.theme = 'light';
     const textContainer = document.createElement('div');
     textContainer.setAttribute('id', 'text-container');
+    textContainer.setAttribute('data-theme', 'light');
 
     // Build projects
     const flowProject = new Project
-    (
-        {imageId: ['flow-visualizer-gif-container']},
-        {pText: [
-        `\
+        (
+            { title: ['Flow on an Angle Grid'] },
+            { imageId: ['flow-visualizer-gif-container'] },
+            {
+                pText: [
+                    `\
         I've been working on a flow simulator to incorporate into 2D games. While a true fluid simulation would take into account density, viscosity etc., \
         this simpler approximation uses a predefined grid of velocities that are multiplied with the current velocity of each particle.
         Having control of the flow field allows flexibility in how this can be used - randomly placed pulses with textures applied to the particles \
         can be used for floating/falling objects, or a static emitter can form a stream for water or lightning.\
         `
-        ]},
-        {fText: [
-        `\
+                ]
+            },
+            {
+                fText: [
+                    `\
         This image shows a vector field overlayed with a particle emitter. The compass needles show the direction of the flow.\
         The angles of the needles are calculated from a Simplex noise image (similar to classic Perlin noise) to give its pseudo-random gradient.\
         `
-        ]}
-    );
+                ]
+            }
+        );
 
     const noiseProject = new Project
-    (
-        {imageId: ['noise-gif-container']},
-        {pText:[
-        `\
+        (
+            { title: ['Tileable Simplex Noise'] },
+            { imageId: ['noise-gif-container'] },
+            {
+                pText: [
+                    `\
         The following image uses the same Simplex noise algorithm but at a much more fine-grained scale. \
         The values that created the angles in the previous image are represented here as a gradient between black and white.\
         Creating a static image is fairly easy - 2D noise is needed, and you can use simple coordinates as the inputs for the noise.\
@@ -128,76 +130,95 @@ function buildProjects()
         The noise that would make up the image had to be mapped to a cylinder first, so that the X coordinate would be circular (end up back where it started)\
         Then, this cylinder had to be bent together at the end, creating a torus, to give a final image that can loop in any 2D direction.\
         `
-        ]},
-        {fText: [
-        `\
+                ]
+            },
+            {
+                fText: [
+                    `\
         This image shows Simplex noise looping over the X and Y axis. It's worth noting that one of the dimensions can be substituted\
         for time, allowing permutation of the noise - but if you're creating tileable noise, you'll run out of dimensions quickly if your noise engine\
         can't support 5+ dimensions.\
         `
-        ]}
-    );
+                ]
+            }
+        );
 
     const noiseTimeProject = new Project
-    (
-        {imageId: ['noise-time-container']},
-        {pText: [
-        `\
+        (
+            { title: ['Simplex Over Time'] },
+            { imageId: ['noise-time-container'] },
+            {
+                pText: [
+                    `\
         The following image uses the same Simplex noise width an extra modifier added for the time dimensions. \
         As you can see it doesn't loop cleanly - for now my workaround is to reverse the animation after some time. \
         Currently the engine only supports up to 4D noise. The colour values here are clamped to give sharper edges.
-        `, 
-        `Here's the noise in action:
+        `,
+                    `Here's the noise in action:
         `
-        ]},
-        {fText: []}
-    );
-    
+                ]
+            },
+            { fText: [] }
+        );
+
     const gameSceneProject = new Project
-    (
-        {imageId: ['game-scene-container']},
-        {pText: [' ']},
-        {fText: [
-        `\
+        (
+            { title: ['Scene with Noise'] },
+            { imageId: ['game-scene-container'] },
+            { pText: [' '] },
+            {
+                fText: [
+                    `\
         In this image you can see the tilemap scene and the noise overlays. This is being written using the SFML library.\
         `
-        ]}
-    );
+                ]
+            }
+        );
 
     const shaderProject = new Project
-    (
-        {imageId: ['shader-container']},
-        {pText: [
-            `\
+        (
+            { title: ['Scene with Light and Normals'] },
+            { imageId: ['shader-container'] },
+            {
+                pText: [
+                    `\
             Here I've added a point light source and a normal map to the tilemap. This makes a marked difference in the perceived quality of the graphics.
             `
-        ]},
-        {fText: [
-            `\
+                ]
+            },
+            {
+                fText: [
+                    `\
             Characters in a snake formation with a light source attached, showing reflection of light off of surface normals.
             `
-        ]}
-    )
+                ]
+            }
+        )
 
     const rayTracerProject = new Project
-    (
-        {imageId: ['ray-tracer-container']},
-        {pText: [
-        `\
+        (
+            { title: ['Raytracer with Spheres'] },
+            { imageId: ['ray-tracer-container'] },
+            {
+                pText: [
+                    `\
         Moving on to something a little different.
         I've been working through a wonderful course called Ray Tracing in One Weekend by Peter Shirley (see References tab).\
         This is a very low level implementation - no textures or models are used, all objects in the scene are drawn using scattering and surface normals.\
         I've been adapting the architechture to work with SFML (OpenGL) and plan on adding caustics before moving on to different shapes, lighting, and speed improvements.
         `
-        ]},
-        {fText: [
-        `\
+                ]
+            },
+            {
+                fText: [
+                    `\
         Here you can see a raytraced scene of randomly populated spheres with three types of scattering - lambertian (diffuse), \
         dialectric (glass), and reflective (metal).\
         `
-        ]}
-    );
-    
+                ]
+            }
+        );
+
     // Adds end of page text, since the scrollbar is hidden.
     const pEnd = document.createElement('p');
     pEnd.classList.add('end', 'fine-print');
@@ -215,46 +236,48 @@ function buildProjects()
     );
 
     // Assign grid row start style to each element in container
-    for(var i = 0; i < textContainer.childElementCount; i++)
-    {
+    for (var i = 0; i < textContainer.childElementCount; i++) {
         textContainer.children[i].style.gridRowStartCount = Number(i) + 1;
     }
 
     // Append to parent containers
     projectsContainer.append(textContainer);
-    projectsContainer.classList.add('hidden');      
+    projectsContainer.classList.add('hidden');
     mainContainer.append(projectsContainer);
 
     // Create mini-map of projects page
     const minimapContainer = document.createElement('div');
     minimapContainer.setAttribute('id', 'minimap-container');
+    minimapContainer.setAttribute('data-theme', 'light');
 
     // Create a duplicate of the main container
     const minimap = document.createElement('div');
     minimap.setAttribute('id', 'minimap');
+    minimap.setAttribute('data-theme', 'light');
     minimap.innerHTML = textContainer.innerHTML;
 
-    const resizeScrollBar = () =>
-    {
+    // Create a box to highlight the visible area
+    const visibleBox = document.createElement('div');
+    visibleBox.setAttribute('id', 'scroll-box');
+    visibleBox.setAttribute('data-theme', 'light');
+
+    const resizeScrollBar = () => {
         minimap.style.width = `${mainContainer.clientWidth}px`;
         minimap.style.height = `${mainContainer.clientHeight}px`;
 
         // Define computed CSS styles for use here and now
-        function vh(percent)
-        {
+        function vh(percent) {
             var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
             return (percent * h) / 100;
         }
-        function vw(percent)
-        {
+        function vw(percent) {
             var w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
             return (percent * w) / 100;
         }
-        function vmin(percent)
-        {
+        function vmin(percent) {
             return Math.min(vh(percent), vw(percent));
         }
-    
+
         // Define ratio of original container to minimap div and transform scale, position
         const scaleWidth = vmin(6) / mainContainer.clientWidth;
         const scaleHeight = vh(89) / textContainer.lastElementChild.offsetTop;
@@ -263,41 +286,69 @@ function buildProjects()
         // offset by the original height minus the ratio of the new height to the last element height, divided by two.
         const offsetY = (mainContainer.clientHeight / 2) - ((vh(89) * scaleHeight) / 2);
         minimap.style.transform = `translate(-${offsetX}px, -${offsetY}px) scale(${scaleWidth}, ${scaleHeight})`;
+
+        // Set scrollbox size
+        const visibleHeight = textContainer.clientHeight * scaleHeight;
+        visibleBox.style.height = `${visibleHeight}px`;
+    }
+    const setScrollBoxPosition = () => {
+        // Define computed CSS styles for use here and now
+        function vh(percent) {
+            var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            return (percent * h) / 100;
+        }
+        const scaleHeight = vh(89) / textContainer.lastElementChild.offsetTop;
+        var scrollPos = textContainer.scrollTop * scaleHeight + vh(9);
+        if (scrollPos + visibleBox.clientHeight > vh(98)) { scrollPos = vh(98) - visibleBox.clientHeight; }
+        visibleBox.style.top = `${scrollPos}px`;
     }
     resizeScrollBar();
+    setScrollBoxPosition();
     // Append containers
     minimapContainer.append(minimap);
+    minimapContainer.append(visibleBox);
     projectsContainer.append(minimapContainer);
 
     // Resizes the scrollbar dynamically
     window.addEventListener('resize', resizeScrollBar);
-    // Event listener that changes the title based on scroll position
-    // TODO: make this generic and scalable
-    textContainer.addEventListener('scroll', () => 
+
+    // Scroll by click
+    minimapContainer.addEventListener('mousedown', (e) =>
     {
-        if (!projectsContainer.classList.contains('hidden'))
+        // Define computed CSS styles for use here and now
+        function vh(percent)
         {
-            const img1HiddenValue = flowProject.image.offsetTop + flowProject.image.clientHeight;
-            const img2HiddenValue = noiseProject.image.offsetTop + noiseProject.image.clientHeight;
-            const img3HiddenValue = noiseTimeProject.image.offsetTop + noiseTimeProject.image.clientHeight;
-            const img4HiddenValue = gameSceneProject.image.offsetTop + gameSceneProject.image.clientHeight;
-            const img5HiddenValue = shaderProject.image.offsetTop + shaderProject.image.clientHeight;
-            const img6HiddenValue = rayTracerProject.image.offsetTop + rayTracerProject.image.clientHeight;
-            if      (textContainer.scrollTop < img1HiddenValue) { title.innerText = "Flow on an Angle Grid"; }
-            else if (textContainer.scrollTop < img2HiddenValue) { title.innerText = "Tileable Simplex Noise"; }
-            else if (textContainer.scrollTop < img3HiddenValue) { title.innerText = "Simplex Over Time"; }
-            else if (textContainer.scrollTop < img4HiddenValue) { title.innerText = "Scene with Noise"; }
-            else if (textContainer.scrollTop < img5HiddenValue) { title.innerText = "Scene with Light and Normals"; }
-            else if (textContainer.scrollTop < img6HiddenValue) { title.innerText = "Raytracer with Spheres"; }
+            var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+            return (percent * h) / 100;
+        }
+        const scaleHeight = vh(89) / textContainer.lastElementChild.offsetTop;
+        textContainer.scrollTop = ((e.pageY - vh(9)) / scaleHeight) - ((visibleBox.clientHeight / scaleHeight) / 2);
+
+        setScrollBoxPosition();
+    });
+
+    // Event listener that changes the title based on scroll position and moves scrollbox
+    textContainer.addEventListener('scroll', () => {
+        setScrollBoxPosition();
+        if (!projectsContainer.classList.contains('hidden')) {
+            for (var i = 0; i < textContainer.childElementCount; i++) {
+                // for every image
+                if (textContainer.childNodes[i].classList.contains('is-image')) {
+                    const hideValue = textContainer.childNodes[i].offsetTop + textContainer.childNodes[i].clientHeight;
+                    if (textContainer.scrollTop < hideValue) {
+                        title.innerText = textContainer.childNodes[i].title;
+                        break;
+                    }
+                }
+            }
         }
     });
 };
 
-function buildReferences() 
-{
+function buildReferences() {
     // Create containers
     const mainContainer = document.getElementById('main-container');
-    const referencesContainer = document.createElement('div');        
+    const referencesContainer = document.createElement('div');
     referencesContainer.setAttribute('id', 'references');
     referencesContainer.dataset.theme = 'light';
     const textContainer = document.createElement('div');
@@ -331,8 +382,7 @@ function buildReferences()
     mainContainer.append(referencesContainer);
 };
 
-function buildEmployers()
-{
+function buildEmployers() {
     // Build containers
     const mainContainer = document.getElementById('main-container');
     const employersContainer = document.createElement('div');
@@ -364,8 +414,7 @@ function buildEmployers()
     mainContainer.append(employersContainer);
 };
 
-function buildPages()
-{
+function buildPages() {
     // Sets homepage to visible
     const mainContainer = document.getElementById('main-container');
     mainContainer.style.opacity = 0;
