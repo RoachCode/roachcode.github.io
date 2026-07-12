a32e2ee0f264c2b36b9b7798c9d5a264ddb20d95
Bradley <74122730+RoachCode@users.noreply.github.com>
2026-07-12 12:52:26 -0700

    reorganize stylesheets into folder

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // index.js
 import { mountResponsiveBackground } from './celticBox.js';
 import { celticBackgroundData } from './config.js';
-import { renderCharacterPage, renderMapPage, renderBattlePage, renderGlossaryPage } from './pages.js';
+import { renderCharacterPage, renderMapPage, renderBattlePage } from './pages.js';
+import { renderGlossaryPage } from './glossary.js';
 
 // 1. Grab DOM elements
 const bgLayer = document.getElementById('background-layer');
